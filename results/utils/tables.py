7d44aa7769540178d9f01a636b10013d4edae1c7
jeffmur <jeffmur@uw.edu>
2024-10-10 21:31:36 -0700

    Compute all experiments, need to redo one

--- results/utils/tables.py
+++ results/utils/tables.py
@@ -20,7 +20,7 @@ def verbose_md_table(pathToAssertion:str) -> Markdown:
     Returns a markdown table with the similarity scores for each metric.
     """
     rows = []
-    rows.append("Similarity (frameCount) | Score [%] | Mean FHE Absolute Error | Pre-processing (s) | Encryption (ms) | FHE Compute (ms) | Plaintext Compute (ms) | FHE/Plain Compute Growth (ms) [%]")
+    rows.append("Similarity [frameCount] | Score [%] | Mean FHE Absolute Error | Pre-processing (s) | Encryption (ms) | FHE Compute (ms) | Plaintext Compute (ms) | FHE/Plain Compute Growth (ms) [%]")
     rows.append("---|---|---|---|---|---|---|---")
     for frameCount in ['all', 'firstLast', 'randomHalf']:
         kld, bhattacharyya, cramer = mean_scores(pathToAssertion, frameCounts=[frameCount]).values()
@@ -40,9 +40,9 @@ def verbose_md_table(pathToAssertion:str) -> Markdown:
         kld_diff, bhattacharyya_diff, cramer_diff = compute_diff_dict(**_params).values()
         kld_diff_perc, bhattacharyya_diff_perc, cramer_diff_perc = compute_growth_percentage_dict(**_params).values()
 
-        rows.append(f"KLD ({frameCount}) | {kld:.2e} [{kld_perc:.2f}%] | {kld_err:.2e} | {pp:.2f} | {kld_enc:.2f} | {kld_fhe:.2f} | {kld_pt:.2f} | {kld_diff:.2f} [{kld_diff_perc:.2f}%]")
-        rows.append(f"Cramer ({frameCount}) | {cramer:.2e} [{cramer_perc:.2f}%] | {cramer_err:.2e} | {pp:.2f} | {cramer_enc:.2f} | {cramer_fhe:.2f} | {cramer_pt:.2f} | {cramer_diff:.2f} [{cramer_diff_perc:.2f}%]")
-        rows.append(f"BC ({frameCount}) | {bhattacharyya:.2e} [{bhattacharyya_perc:.2f}%] | {bhattacharyya_err:.2e} | {pp:.2f} | {bhattacharyya_enc:.2f} | {bhattacharyya_fhe:.2f} | {bhattacharyya_pt:.2f} | {bhattacharyya_diff:.2f} [{bhattacharyya_diff_perc:.2f}%]")
+        rows.append(f"KLD [{frameCount}] | {kld:.2e} [{kld_perc:.2f}%] | {kld_err:.2e} | {pp:.2f} | {kld_enc:.2f} | {kld_fhe:.2f} | {kld_pt:.2f} | {kld_diff:.2f} [{kld_diff_perc:.2f}%]")
+        rows.append(f"Cramer [{frameCount}] | {cramer:.2e} [{cramer_perc:.2f}%] | {cramer_err:.2e} | {pp:.2f} | {cramer_enc:.2f} | {cramer_fhe:.2f} | {cramer_pt:.2f} | {cramer_diff:.2f} [{cramer_diff_perc:.2f}%]")
+        rows.append(f"BC [{frameCount}] | {bhattacharyya:.2e} [{bhattacharyya_perc:.2f}%] | {bhattacharyya_err:.2e} | {pp:.2f} | {bhattacharyya_enc:.2f} | {bhattacharyya_fhe:.2f} | {bhattacharyya_pt:.2f} | {bhattacharyya_diff:.2f} [{bhattacharyya_diff_perc:.2f}%]")
 
     return Markdown(textwrap.dedent('\n'.join(rows)))
 
